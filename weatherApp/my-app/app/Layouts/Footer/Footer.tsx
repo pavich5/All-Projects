7270834d77a92c2b17e5b1e9d1c8775b1e78ca14
pavich5 <pavic.antonio969@gmail.com>
2023-10-17 12:41:43 +0200

    a

--- weatherApp/my-app/app/Layouts/Footer/Footer.tsx
+++ weatherApp/my-app/app/Layouts/Footer/Footer.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import './Footer.css';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="footer">
+      <div className="footer-content">
+        <p>&copy; 2023 Pokémon Company</p>
+        <div className="social-icons">
+          <a href="https://www.facebook.com/pokemon" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-facebook"></i>
+          </a>
+          <a href="https://twitter.com/Pokemon" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-twitter"></i>
+          </a>
+          <a href="https://www.instagram.com/pokemon/" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-instagram"></i>
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
--- weatherApp/my-app/app/pokemons/page.tsx
+++ weatherApp/my-app/app/pokemons/page.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./Pokemons.css";
-import PokemonCard from "../components/PokemonCart";
-import Link from "next/link";
 import PokemonList from "../components/PokemonList";
 
-const offset = 20;
-async function getPokemonsData() {
-  const res = await fetch(`https://pokeapi.co/api/v2/pokemon?${offset}=60&limit=20`);
+let offset = 10;
+async function getPokemonsData(offset: number) {
+  const res = await fetch(
+    `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=10`
+  );
   const data = await res.json();
   if (!res.ok) {
     throw new Error("Failed to fetch data");
@@ -14,17 +14,35 @@ async function getPokemonsData() {
   return data;
 }
 
-
-const weather = async () => {
-  const data = await getPokemonsData();
+const Pokemons = async () => {
+  const data = await getPokemonsData(offset);
   return (
     <div className="pokemonContainer">
-    <PokemonList data={data} />
-    <button>Next</button>
-    <button>Previous</button>
-  </div>
+      <PokemonList data={data} />
+      {/* <ButtonsWrapper /> */}
+    </div>
   );
 };
 
+export default Pokemons;
 
-export default weather;
+
+
+
+// const ButtonsWrapper = () => {
+//   const handleNextClick = () => {
+//     offset += 20;
+//     getPokemonsData(offset);
+//   };
+
+//   const handlePrevClick = () => {
+//     offset -= 20;
+//     getPokemonsData(offset);
+//   };
+//   return (
+//     <div>
+//       <button onClick={handlePrevClick}>Previous Page</button>
+//       <button onClick={handleNextClick}>Next Page</button>;
+//     </div>
+//   );
+// };
