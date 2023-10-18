9c355e9a6d57251e8c7c38bb2667883ab094df01
pavich5 <pavic.antonio969@gmail.com>
2023-10-18 11:17:54 +0200

    d

--- PokemonApps/my-app/app/components/PokemonList.tsx
+++ PokemonApps/my-app/app/components/PokemonList.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import React from 'react';
 import PokemonCard from './PokemonCart';
 
-const PokemonList = ({data}) => {
+const PokemonList = (data:any) => {
 
   return (
     <div className="pokemonList">
